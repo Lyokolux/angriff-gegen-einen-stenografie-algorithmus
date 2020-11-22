e4b74b30a42b3adba182dc824e3c420a1c19e781
Lyokolux <lyokolux@protonmail.com>
2020-11-22 22:55:24 +0100

    ✨(frequencies.py): add some frequencies

---
+++ src/frequencies.py
@@ -0,0 +1,37 @@
+# Made by Stefan Trost
+# available on https://www.sttmedia.com/characterfrequency-german
+GERMAN_LETTER_FREQUENCIES = {
+    'E': 16.93,
+    'N': 10.53,
+    'I': 8.02,
+    'R': 6.89,
+    'S': 6.42,
+    'T': 5.79,
+    'A': 5.58,
+    'D': 4.98,
+    'H': 4.98,
+    'U': 3.83,
+    'L': 3.60,
+    'C': 3.16,
+    'G': 3.02,
+    'M': 2.55,
+    'O': 2.24,
+    'B': 1.96,
+    'W': 1.78,
+    'F': 1.49,
+    'K': 1.32,
+    'Z': 1.21,
+    'V': 0.84,
+    'P': 0.67,
+    'Ü': 0.65,
+    'Ä': 0.54,
+    'ß': 0.37,
+    'Ö': 0.30,
+    'J': 0.24,
+    'X': 0.05,
+    'Y': 0.05,
+    'Q': 0.02
+}
+
+# Because the dictionaries preserve the ordering since version 3.6
+SORTED_BY_FREQUENCIES = list(GERMAN_LETTER_FREQUENCIES.keys())
