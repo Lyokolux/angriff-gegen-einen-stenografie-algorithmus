7e5607ae51ce675c83a3186606c4f411409511d4
Lyokolux <lyokolux@protonmail.com>
2020-11-22 23:09:21 +0100

    ✨(substitutionschiffre.py): generate a random key and encrypt a message with it. All with bytes instead of string

---
+++ src/substitutionschiffre.py
@@ -0,0 +1,25 @@
+from random import shuffle
+from typing import List, Tuple, MutableMapping
+from frequencies import SORTED_BY_FREQUENCIES
+
+
+def generateKey() -> List[int]:
+    key = list(range(256))
+    shuffle(key)
+    return key
+
+
+def encryptMessage(message: bytearray, key: List[int]) -> List[int]:
+    return [key[c] for c in message]
+
+
+def encrypted_to_string(message: List[int]):
+    return ''.join([chr(x) for x in encrypted_message])
+
+
+if __name__ == "__main__":
+    input_message = bytearray('abcdefghijklmnopqrstuvwxyzäöüß', 'utf-8')
+    key = generateKey()
+    encrypted_message = encryptMessage(input_message, key)
+    print(encrypted_message)
+    print(encrypted_to_string(encrypted_message))
