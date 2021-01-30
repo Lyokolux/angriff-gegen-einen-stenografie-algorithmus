cac312e686615db4c5a3882147a255159c66370c
flololan <flo.s.weiss@gmail.com>
2021-01-30 09:59:39 +0100

    Added Print feedback

--- src/benchmark_throughput.py
+++ src/benchmark_throughput.py
@@ -4,6 +4,8 @@ file_src = 'src/assets/1mB.txt'
 image_src = 'src/assets/cover_image_coin.png'
 repetition = 100
 
+print("Please wait...")
+
 t = Timer(stmt="[choice(encryption_table[c]) for c in msg]",
           setup=f"from image import Image; from random import choice; content = open('{file_src}').read(); msg = [ord(c) for c in content]; encryption_table = Image('{image_src}').getEncryptionTable();"
           )
