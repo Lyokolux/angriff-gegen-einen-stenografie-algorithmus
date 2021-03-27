275e0d20b1700c6105b002b31c181b795bfe4718
Lyokolux <lyokolux@protonmail.com>
2021-03-27 11:12:48 +0100

    feat(implementierung): add text

--- src/implementierung.py
+++ src/implementierung.py
@@ -29,5 +29,9 @@ if __name__ == "__main__":
     encrypted = encrypt(key, msg)
     decrypted = decrypt(key, encrypted)
 
+    print(f"Nachricht: {msg}")
+    print(f"Verschlüsselt: {encrypted} -- mit {imagePath}")
+    print(f"Entschlüsselt: {decrypted}")
+
     # Ensure that the encryption is working as expected
     assert score_it(msg, decrypted) == 0
